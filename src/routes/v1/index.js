c8ca2d4d3805e0222b757df7bbf754ca8c2357fb
Surya Tanwar <forsurya02@gmail.com>
2024-02-07 05:36:32 +0530

    Added isAuthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
-
+const { AuthRequestMiddlewares } = require("../../middlewares")
 const {InfoController} = require("../../controllers");
 const userRoutes = require("./user-routes");
 
-router.get("/info", InfoController.info);
+router.get("/info", AuthRequestMiddlewares.checkAuth, InfoController.info);
 
 router.use("/user", userRoutes);
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 const { UserRepository } = require("../repositories");
 const AppError = require("../utils/errors/app-error");
 const { Auth } = require("../utils/common");
+const user = require("../models/user");
 
 const userRepo = new UserRepository();
 
@@ -40,9 +41,28 @@ async function signin(data) {
     }
 }
 
-
+async function isAuthenticated(token) {
+    try {
+        if(!token) {
+            throw new AppError("Missing JWT token", StatusCodes.BAD_REQUEST);
+        }
+        const response = Auth.verifyToken(token);
+        const user = await userRepo.get(response.id);
+        if(!user) {
+            throw new AppError("No user found", StatusCodes.NOT_FOUND);
+        }
+        return user.id;
+    } catch (error) {
+        if(error instanceof AppError) throw error;
+        if(error.name == "JsonWebTokenError") {
+            throw new AppError("Invalid JWT token", StatusCodes.BAD_REQUEST);
+        }
+        throw new AppError("Something went wrong", StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
 
 module.exports = {
     create,
-    signin
+    signin,
+    isAuthenticated
 }
